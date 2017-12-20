c204a4c04d0e2bc42d8f29d363bae0ec855278f1
˜slbruno˜ <brunolagoela@gmail.com>
2017-12-20 18:16:41 -0200

    parece ter funcionado

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module Ytpush
     config.load_defaults 5.1
     Yt.configuration.api_key = ENV["YOUTUBE_API_KEY"]
     Yt.configuration.log_level = :debug
-    Yt.configuration.client_id = ENV["YOUTUBE_CLIENT_ID"]
-    Yt.configuration.client_secret = ENV["YOUTUBE_CLIENT_SECRET"]
+    #Yt.configuration.client_id = ENV["YOUTUBE_CLIENT_ID"]
+    #Yt.configuration.client_secret = ENV["YOUTUBE_CLIENT_SECRET"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
