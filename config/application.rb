bb073712cc93cabb56f2ea3da16e9cbd10531e55
˜slbruno˜ <brunolagoela@gmail.com>
2017-12-20 14:06:50 -0200

    yt debug

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Ytpush
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     Yt.configuration.api_key = ENV["YOUTUBE_API_KEY"]
+    Yt.configuration.log_level = :debug
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
