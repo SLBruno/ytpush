42cc6856bd915132e17db2957e0c8cc7f0b6f11f
˜slbruno˜ <brunolagoela@gmail.com>
2017-12-20 14:25:03 -0200

    youtube client id inserido3

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Yt.configure do |config|
+  config.api_key = ENV["YOUTUBE_API_KEY"]
+  config.client_id = ENV["YOUTUBE_CLIENT_ID"]
+  config.client_secret = ENV["YOUTUBE_CLIENT_SECRET"]
+end
+
 module Ytpush
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    Yt.configuration.api_key = ENV["YOUTUBE_API_KEY"]
+    #Yt.configuration.api_key = ENV["YOUTUBE_API_KEY"]
     Yt.configuration.log_level = :debug
-    Yt.configuration.client_id = ENV["YOUTUBE_CLIENT_ID"]
-    Yt.configuration.client_secret = ENV["YOUTUBE_CLIENT_SECRET"]
+    #Yt.configuration.client_id = ENV["YOUTUBE_CLIENT_ID"]
+    #Yt.configuration.client_secret = ENV["YOUTUBE_CLIENT_SECRET"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
